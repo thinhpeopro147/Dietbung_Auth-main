d7679adf4cccfc8208d8ac8ce4690f5a70198eb9
thinhpeopro147 <145535325+thinhpeopro147@users.noreply.github.com>
2026-06-15 19:00:32 +0700

    fix seen icon smaller

--- frontend/src/components/chat/MessageItem.tsx
+++ frontend/src/components/chat/MessageItem.tsx
@@ -106,7 +106,7 @@ const MessageItem = ({
                             type="chat"
                             name={participant?.displayName ?? ""}
                             avatarUrl={participant?.avatarUrl ?? undefined}
-                            className="size-2"
+                            className="size-1"
                           />
                         );
                       })}
@@ -130,7 +130,7 @@ const MessageItem = ({
                               type="chat"
                               name={participant?.displayName ?? ""}
                               avatarUrl={participant?.avatarUrl ?? undefined}
-                              className="size-2"
+                              className="size-1"
                             />
                           );
                         })
